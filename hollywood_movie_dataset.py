ace3271dd701c378a48b5ba50cabf688feb3b0d1
SofiaAbihsera <sofiaabih@icloud.com>
2025-10-16 11:08:27 -0400

    histogram_rotten_tomatoes
    graph #1 histogram

--- hollywood_movie_dataset.py
+++ hollywood_movie_dataset.py
@@ -1,5 +1,10 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 hollywood_movies = pd.read_csv("movies.csv")
 
 print("Hollywood movie dataset")
+
+
+
+plt.hist(hollywood_movies["Rotten Tomatoes %"])
