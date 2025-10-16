e3388a41eb4b2efb6be51a59a187896bae4740a5
SofiaAbihsera <sofiaabih@icloud.com>
2025-10-16 11:31:50 -0400

    part_4(h)
    1 histogram

--- hollywood_movie_dataset.py
+++ hollywood_movie_dataset.py
@@ -5,6 +5,18 @@ hollywood_movies = pd.read_csv("movies.csv")
 
 print("Hollywood movie dataset")
 
-
+#Part 4. H 
 
 plt.hist(hollywood_movies["Rotten Tomatoes %"])
+
+import numpy as np 
+
+
+x=np.array([0.,20.,40.6,80.1])
+y=np.array([0,2,8,10])
+
+plt.plot(x,y)
+plt.xlabel("Rotten Tomatoes %")
+plt.ylabel("Number of movies")
+plt.title("corellation of rottan tomatoes and number of movies")
+plt.show()
